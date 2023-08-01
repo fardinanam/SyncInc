7c116602e67354385d214316edd4282b1380dc12
ehsankader16 <1805067@ugrad.cse.buet.ac.bd>
2023-08-02 02:17:59 +0600

    backend support needed for login, register

--- Front-end/syncinc/src/App.js
+++ Front-end/syncinc/src/App.js
@@ -1,19 +1,25 @@
-import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import React, {createContext, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import { Home } from "./containers/Home";
 import { Login } from "./containers/Login";
 import { Register } from "./containers/Register";
 
+export const LoginContext = createContext();
+
 function App() {
-
-
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <LoginContext.Provider value={[loggedIn, setLoggedIn]}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+    </LoginContext.Provider>
+    
   );
 }
 export default App;
--- Front-end/syncinc/src/containers/Register.js
+++ Front-end/syncinc/src/containers/Register.js
@@ -1,18 +1,48 @@
 import React, { useState } from "react";
 import {Link} from 'react-router-dom';
 import '../styles/auth.css'
+import axios from "axios";
+
+const baseUrl = 'http://localhost:8000/';
 
 export const Register = (props) => {
     const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+    const [password, setPassword] = useState('');
     const [firstname, setFirstName] = useState('');
     const [lastname, setLastName] = useState('');
     const [username, setUserName] = useState('');
     const [phone, setPhone] = useState('');
 
+    const isValidEmail = (email) => {
+        return /\S+@\S+\.\S+/.test(email);
+    }
+
+    const isValidPassword = (password) => {           
+        return /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[#?!_@$%^&*-])(?=.{8,})/.test(password);
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(email);
+        if(isValidEmail(email) === false) {
+            alert("Please enter a valid email address.");
+            return;
+        }
+
+        if(isValidPassword(password) === false) {
+            alert("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.");
+            return;
+        }
+        
+        axios.post(baseUrl + 'accounts/register/', {
+            first_name: firstname,
+            last_name: lastname,
+            username: username,
+            email: email,
+            phone: phone,
+            password: password,
+        }).then((response) => {
+            console.log(response.data);
+        })
     }
 
     return (
@@ -20,17 +50,17 @@ export const Register = (props) => {
             <h2>Register</h2>
             <form className="register-form" onSubmit={handleSubmit}>
                 <label htmlFor="firstname">First Name</label>
-                <input value={firstname} name="firstname" onChange={(e) => setFirstName(e.target.value)} id="firstname" placeholder="First Name" />
+                <input value={firstname} name="firstname" onChange={(e) => setFirstName(e.target.value)} id="firstname" placeholder="First Name" required/>
                 <label htmlFor="lastname">Last Name</label>
-                <input value={lastname} name="lastname" onChange={(e) => setLastName(e.target.value)} id="lastname" placeholder="Last Name" />
+                <input value={lastname} name="lastname" onChange={(e) => setLastName(e.target.value)} id="lastname" placeholder="Last Name" required/>
                 <label htmlFor="username">Username</label>
-                <input value={username} name="username" onChange={(e) => setUserName(e.target.value)} id="username" placeholder="Username" />
+                <input value={username} name="username" onChange={(e) => setUserName(e.target.value)} id="username" placeholder="Username" required/>
                 <label htmlFor="email">Email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" required/>
                 <label htmlFor="phone">Phone Number</label>
-                <input value={phone} onChange={(e) => setPhone(e.target.value)} type="tel" placeholder="01xxxxxxxxx" id="phone" name="phone" />
+                <input value={phone} onChange={(e) => setPhone(e.target.value)} type="tel" placeholder="01xxxxxxxxx" id="phone" name="phone" required/>
                 <label htmlFor="password">Password</label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" required/>
                 <button type="submit">Register</button>
             </form>
             <p className="link-btn">Already have an account? <Link to="/login">Login here</Link></p>
