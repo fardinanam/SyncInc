a68c4a2028e53fec3c689eb24bc1cdaae3b2a3e9
Fardin Anam Aungon <52667920+fardinanam@users.noreply.github.com>
2023-08-13 11:39:51 +0600

    commit for deployment test 1

--- Front-end/syncinc/src/utils/config.js
+++ Front-end/syncinc/src/utils/config.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'syncinc.onrender.com';
+export const baseUrl = 'https://syncinc.onrender.com/';
 export const refreshTokenDelay = 1000 * 60 * 4; // 5 minutes, set in django settings.py
