3310066f0e856ce276d90391922db9138fc6a694
Fardin Anam Aungon <52667920+fardinanam@users.noreply.github.com>
2023-08-28 07:40:35 +0600

    Added icon instead of names to project card roles

--- Front-end/syncinc/src/components/ProjectCard.js
+++ Front-end/syncinc/src/components/ProjectCard.js
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import { Typography, Box, Chip } from "@mui/material";
+import { Typography, Box, Chip, Stack, Tooltip } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import notifyWithToast from '../utils/toast';
 import LockRoundedIcon from '@mui/icons-material/LockRounded';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
 
 const ProjectCard = (props) => {
     const theme = useTheme();
@@ -54,10 +57,28 @@ const ProjectCard = (props) => {
                     {name && name}
                 </Typography>
                 {roles?.length > 0 ? 
-                    roles?.includes("Project Leader") ?
-                    <Chip color='success' label="Leader" size='small' /> :
-                    <Chip color='primary' label="Member" size='small' /> :
-                    <LockRoundedIcon color='disabled' size='small'/>
+                    <Stack 
+                        display='flex'
+                        direction='row'
+                        justifyContent='flex-start'
+                        spacing={0.5}
+                    >
+                        {roles?.includes("Admin") && <Tooltip title="Admin"> 
+                                <AdminPanelSettingsIcon size="small" color='success'/>
+                            </Tooltip>
+                        }
+                        {roles?.includes("Project Leader") && <Tooltip title="Leader"> 
+                                <ManageAccountsIcon size="small" color='primary'/>
+                            </Tooltip>
+                        }
+                        {roles?.includes("Assignee") && <Tooltip title="Assignee">
+                                <SupervisedUserCircleIcon size="small" color='secondary'/>
+                            </Tooltip>
+                        }
+                    </Stack>
+                    : <Tooltip title="Private"> 
+                        <LockRoundedIcon size="small" color='disabled'/>
+                    </Tooltip>
                 }
                 </Box>
                 <Typography
