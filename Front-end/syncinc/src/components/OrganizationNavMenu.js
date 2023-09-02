1a4b377a875f32e458f3316b077a753685cffbf8
ehsankader16 <1805067@ugrad.cse.buet.ac.bd>
2023-09-02 23:17:06 +0600

    OrganizationNavMenu and SearchSuggestion edited

--- Front-end/syncinc/src/components/OrganizationNavMenu.js
+++ Front-end/syncinc/src/components/OrganizationNavMenu.js
@@ -5,12 +5,10 @@ import { Box } from '@mui/material';
 const OrganizationNavMenu = ({organization_id}) => {
     const navigate = useNavigate();
 
-    const menuItems = ["Projects", "Employees", "Vendors"];
+    const menuItems = ["Projects", "Employees"];
     const handleMenuSelect = (menu) => {
         if(menu.toLowerCase() === "projects")
             navigate(`/organization/${organization_id}/projects`);
-        else if(menu.toLowerCase() === "vendors")
-            navigate(`/organization/${organization_id}/vendors`);
         else if(menu.toLowerCase() === "employees")
             navigate(`/organization/${organization_id}/employees`);
     }
