33b673e6637a18ceff1103018ff2f798ba6f3d2c
Fardin Anam Aungon <52667920+fardinanam@users.noreply.github.com>
2023-09-06 01:48:18 +0600

    Using env variables

--- Front-end/syncinc/src/utils/validators.js
+++ Front-end/syncinc/src/utils/validators.js
@@ -9,4 +9,17 @@ const isValidPassword = (password) => {
 const isValidNumber = (number) => {
     return /^01\d{9}$/.test(number);
 }
+
+const isValidName = (name) => {
+    // should only contain letters and spaces
+    return /^[a-zA-Z\s]+$/.test(name);
+}
+
+const isValidUsername = (username) => {
+    // should only contain letters, numbers, and underscores, period
+    // start with a letter
+
+    return /^[a-zA-Z][a-zA-Z0-9_.]+$/.test(username);
+}
+
 export { isValidEmail, isValidPassword, isValidNumber };
