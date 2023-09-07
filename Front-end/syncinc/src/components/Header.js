7f08f403193a51924e1aa14dd4a0a0e3159f3a7f
ababiltalha <1805077@ugrad.cse.buet.ac.bd>
2023-09-07 22:57:48 +0600

    notification card, and menu with dummy data

--- Front-end/syncinc/src/components/Header.js
+++ Front-end/syncinc/src/components/Header.js
@@ -2,12 +2,15 @@ import React, { useContext } from "react";
 import Logo from "../assets/logo.svg"
 import SearchBar from "./SearchBar";
 import ProfileMenu from "./ProfileMenu";
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 import {ColorModeContext} from '../context/ThemeContext';
 import IconButton from '@mui/material/IconButton';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
+import Badge from '@mui/material/Badge';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 import { useTheme } from '@mui/material/styles';
+import NotificationMenu from "./NotificationMenu";
 
 const Header = () => {   
     const theme = useTheme();
@@ -32,6 +35,14 @@ const Header = () => {
                     SyncInc.
                 </Typography>
                 {/* <SearchBar /> */}
+                {/* <IconButton sx={{ ml: 1 }} onClick={() => {} } color="inherit">
+                    <Badge color="primary" badgeContent={10} max={9}>
+                        <NotificationsIcon
+                            fontSize="medium"
+                        />
+                    </Badge>
+                </IconButton> */}
+                <NotificationMenu />
                 <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
                     {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
                 </IconButton>
